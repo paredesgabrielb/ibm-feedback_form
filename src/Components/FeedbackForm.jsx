6be757f9dab867934c3a3040759062e6ffe3080b
Gabriel Paredes <paredesgabrielb@gmail.com>
2024-10-14 15:04:39 -0400

    complete steps

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
     const [formData, setFormData] = useState({
-        username: '',
+        name: '',
         email: '',
         feedback: ''
     });
@@ -16,19 +16,39 @@ const FeedbackForm = () => {
         });
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const confirmationMessage = `
+            Name: ${formData.username}
+            Email: ${formData.email}
+            Feedback: ${formData.feedback}
+        `;
+        const isConfirmed = window.confirm(`Please confirm your details: \n\n ${confirmationMessage}`);
+                
+        if(isConfirmed){
+            console.log('Submitting feedback: ',formData);
+            setFormData({
+                name: '',
+                email: '',
+                feedback: ''
+            });
+            alert("Thanks you for your valuable feedback!");
+        }
+
+    }
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form onSubmit={handleSubmit} className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         <input 
             type='text' 
-            name='username'
+            name='name'
             placeholder='Your Name'
-            value={formData.username}
+            value={formData.name}
             onChange={handleChange} />
         <input 
             type='email'
